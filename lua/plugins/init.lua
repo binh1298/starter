58c031c14ea9fe7f433ee30c7035e3eb9e807d7a
Pham Duc Binh <binh1298@gmail.com>
2024-11-25 16:16:42 +0700

    init tmuxnav

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -13,12 +13,25 @@ return {
     end,
   },
   {
-    "nvim-tree",
-    enable = false,
-  },
-  {
     "ms-jpq/chadtree",
   },
+  {
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
   {
   	"nvim-treesitter/nvim-treesitter",
 		opts = {
