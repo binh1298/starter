8b51b8d20b41d6cebc3a864f9ee205eb3f35923d
Pham Duc Binh <binh1298@gmail.com>
2024-11-25 22:33:01 -0500

    init lsps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "biome", "lua_ls", "tailwindcss", "eslint" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -17,8 +17,33 @@ for _, lsp in ipairs(servers) do
 end
 
 -- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  init_options = {
+    preferences = {
+      importModuleSpecifierPreference = "non-relative",
+    },
+    server_capabilities = { documentFormattingProvider = false },
+  },
+}
+
+lspconfig.gopls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  rootdir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      -- usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+    },
+  },
+}
