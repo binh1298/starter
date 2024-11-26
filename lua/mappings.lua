e11c6cb5ea3d451dc6cf7e6082bc69228dbd76b8
Pham Duc Binh <binh1298@gmail.com>
2024-11-26 02:43:11 -0500

    add hot key for saving

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -23,3 +23,5 @@ map("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", { desc = "Navigate to the pr
 map("n", "<leader>fr", "<cmd>Telescope lsp_references<CR>", { desc = "Find all references" })
 map("n", "<leader>fi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Find all implementations" })
 map("n", "<leader>fd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Find all definitions" })
+
+map("i", "<C-s>", "<Esc> <C-s> <cmd> w <CR>", { desc = "Save file & Exit Input Mode" })
