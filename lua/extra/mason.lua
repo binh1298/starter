78ce53444b3a2e09ed5e876fb9ba79f42519a574
Pham Duc Binh <binh1298@gmail.com>
2024-11-28 13:19:57 +0700

    ignore some mason stuff

--- lua/extra/mason.lua
+++ lua/extra/mason.lua
@@ -14,26 +14,25 @@ return {
       -- Lua
       -- "stylua",
       -- "lua-language-server",
-
       -- TS / JS
-      "typescript-language-server",
-      "eslint-lsp",
-      "prettierd",
-      "prettier",
-      "eslint_d",
+      -- "typescript-language-server",
+      -- "eslint-lsp",
+      -- "prettierd",
+      -- "prettier",
+      -- "eslint_d",
+      -- Bash
+      -- "bash-language-server",
+      -- "shellcheck",
+      -- YAML
+      -- "yaml-language-server",
       -- Astro
       -- "astro-language-server",
-      -- Bash
-      "bash-language-server",
-      "shellcheck",
-      -- YAML
-      "yaml-language-server",
       -- Cucumber
       -- "cucumber-language-server",
       -- HTML CSS
-      "css-lsp",
-      "html-lsp",
-      "tailwindcss-language-server",
+      -- "css-lsp",
+      -- "html-lsp",
+      -- "tailwindcss-language-server",
       -- Apex
       -- "apex-language-server",
     },
