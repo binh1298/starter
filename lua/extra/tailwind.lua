3f9f7762bc2e8e89b511fbaeccb56d96e3c6c4e8
Pham Duc Binh <binh1298@gmail.com>
2024-11-28 11:30:11 +0700

    fix cmp

--- lua/extra/tailwind.lua
+++ lua/extra/tailwind.lua
@@ -3,30 +3,30 @@ return {
   dependencies = {
     { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
   },
-  opts = function(_, opts)
-    -- taiwind
-    -- original LazyVim kind icon formatter
-    local format_kinds = opts.formatting.format
-    opts.formatting.format = function(entry, item)
-      format_kinds(entry, item) -- add icons
-      return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-    end
-    -- conflict with copilot
-    opts.mapping["<Tab>"] = nil
-    opts.mapping["S-<Tab>"] = nil
-
-    -- rust
-    local M = {
-      sources = {
-        { name = "nvim_lsp", group_index = 2 },
-        -- { name = "copilot", group_index = 2 },
-        { name = "luasnip", group_index = 2 },
-        { name = "buffer", group_index = 2 },
-        { name = "nvim_lua", group_index = 2 },
-        { name = "path", group_index = 2 },
-        { name = "crates", group_index = 2 },
-      },
-    }
-    return M
-  end,
+  -- opts = function(_, opts)
+  --   -- taiwind
+  --   -- original LazyVim kind icon formatter
+  --   local format_kinds = opts.formatting.format
+  --   opts.formatting.format = function(entry, item)
+  --     format_kinds(entry, item) -- add icons
+  --     return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+  --   end
+  --   -- conflict with copilot
+  --   opts.mapping["<Tab>"] = nil
+  --   opts.mapping["S-<Tab>"] = nil
+  --
+  --   -- rust
+  --   local M = {
+  --     sources = {
+  --       { name = "nvim_lsp", group_index = 2 },
+  --       -- { name = "copilot", group_index = 2 },
+  --       { name = "luasnip", group_index = 2 },
+  --       { name = "buffer", group_index = 2 },
+  --       { name = "nvim_lua", group_index = 2 },
+  --       { name = "path", group_index = 2 },
+  --       { name = "crates", group_index = 2 },
+  --     },
+  --   }
+  --   return M
+  -- end,
 }
