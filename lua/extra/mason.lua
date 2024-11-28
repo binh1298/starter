bf193109c630412fcbdfc78d0c0091d0828ddba5
Pham Duc Binh <binh1298@gmail.com>
2024-11-28 11:39:58 +0700

    add some mason libs

--- lua/extra/mason.lua
+++ lua/extra/mason.lua
@@ -17,22 +17,22 @@ return {
 
       -- TS / JS
       "typescript-language-server",
-      -- "eslint-lsp",
-      -- "prettierd",
-      -- "prettier",
-      -- "eslint_d",
+      "eslint-lsp",
+      "prettierd",
+      "prettier",
+      "eslint_d",
       -- Astro
       -- "astro-language-server",
       -- Bash
-      -- "bash-language-server",
-      -- "shellcheck",
+      "bash-language-server",
+      "shellcheck",
       -- YAML
-      -- "yaml-language-server",
+      "yaml-language-server",
       -- Cucumber
       -- "cucumber-language-server",
       -- HTML CSS
-      -- "css-lsp",
-      -- "html-lsp",
+      "css-lsp",
+      "html-lsp",
       "tailwindcss-language-server",
       -- Apex
       -- "apex-language-server",
