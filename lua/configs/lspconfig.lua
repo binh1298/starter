e803f4bd8484cccd451104559568b00adf9a6840
Pham Duc Binh <binh1298@gmail.com>
2025-05-23 15:01:38 +0700

    attempt to fix copilot

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "biome", "lua_ls", "tailwindcss", "eslint", "copilot_ls" }
+local servers = { "html", "cssls", "biome", "lua_ls", "tailwindcss", "eslint", "copilot" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
